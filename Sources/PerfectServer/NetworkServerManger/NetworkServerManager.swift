344d7b6b917f9ac466b8aaf5fb1e8d8a73bc1828
zhangerbing <admin@MacPro.local>
2019-03-04 18:40:33 +0800

    1. Add NetworkServerManager.

---
+++ Sources/PerfectServer/NetworkServerManger/NetworkServerManager.swift
@@ -0,0 +1,97 @@
+//
+//  NetworkServerManager.swift
+//  PerfectServer
+//
+//  Created by zhangerbing on 2019/3/4.
+//
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+open class NetworkServerManager {
+    fileprivate var server: HTTPServer
+    internal init(root: String, port: UInt16) {
+        
+        // 创建HTTPServer
+        self.server = HTTPServer()
+        
+        // 创建路由器
+        var routes = Routes(baseUri: "/api")
+        
+        // 注册路由器
+        configure(routes: &routes)
+        
+        // 路由添加进服务
+        server.addRoutes(routes)
+        // 端口
+        server.serverPort = port
+        // 根目录
+        server.documentRoot = root
+        // 404过滤
+        server.setResponseFilters([(Filter404(), .high)])
+    }
+    
+    // MARK: - 开启服务
+    open func startServer() {
+        do {
+            print("启动HTTP服务器")
+            try server.start()
+        } catch PerfectError.networkError(let error, let msg) {
+            print("网络出现错误：\(error) \(msg)")
+        } catch {
+            print("网络未知错误")
+        }
+    }
+    
+    // MARK: - 注册路由
+    private func configure(routes: inout Routes) {
+        // 添加接口，请求方式，路径
+        routes.add(method: .get, uri: "/") { (request, response) in
+            // 响应头
+            response.setHeader(.contentType, value: "text/html")
+            
+            let jsonDic = ["hello": "world"]
+            
+            let jsonString = self.baseResponseBodyJSONData(status: 200, message: "Success", data: jsonDic)
+            
+            response.setBody(string: jsonString)
+            
+            response.completed()
+        }
+    }
+    
+    // MARK: - 通用响应格式
+    func baseResponseBodyJSONData(status: Int, message: String, data: Any!) -> String {
+        var result = [String: Any]()
+        
+        result.updateValue(status, forKey: "status")
+        result.updateValue(message, forKey: "message")
+        if data != nil {
+            result.updateValue(data, forKey: "data")
+        } else {
+            result.updateValue("", forKey: "data")
+        }
+        
+        guard let jsonString = try? result.jsonEncodedString() else { return "" }
+        return jsonString
+    }
+    
+    // MARK: - 404过滤
+    struct Filter404: HTTPResponseFilter {
+        func filterBody(response: HTTPResponse, callback: (HTTPResponseFilterResult) -> ()) {
+            callback(.continue)
+        }
+        
+        func filterHeaders(response: HTTPResponse, callback: (HTTPResponseFilterResult) -> ()) {
+            if case .notFound = response.status {
+                response.setBody(string: "404 文件\(response.request.path) 不存在")
+                response.setHeader(.contentLength, value: "\(response.bodyBytes.count)")
+                callback(.done)
+            } else {
+                callback(.continue)
+            }
+        }
+        
+    }
+}
--- Sources/PerfectServer/main.swift
+++ Sources/PerfectServer/main.swift
@@ -1,17 +1,6 @@
 import PerfectHTTP
 import PerfectHTTPServer
 
-// 注册自己的路由和请求 / 响应句柄
-var routes = Routes()
-routes.add(method: .get, uri: "/") {
-    request, response in
-    response.setHeader(.contentType, value: "text/html")
-    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, perfect!</body></html>").completed()
-}
+let networkServer = NetworkServerManager(root: "webroot", port: 8888)
+networkServer.startServer()
 
-do {
-    // 启动HTTP服务器
-    try HTTPServer.launch(.server(name: "www.example.ca", port: 8181, routes: routes))
-} catch {
-    fatalError("\(error)")
-}
